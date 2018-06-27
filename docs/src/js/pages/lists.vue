604bb42b813cf6f827e5b1285150910335381de3
SuperDJ <super.derkjan@gmail.com>
2018-06-27 23:27:18 +0200

    Fixed lists

--- docs/src/js/pages/lists.vue
+++ docs/src/js/pages/lists.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <article class="row">
-            <section class="xs12">
-                <h1 class="xs12 text--red-400">Lists</h1>
+            <h1 class="xs12 text--red-400">Lists</h1>
+
+            <section class="xs12 sm6">
+                <p>
+                    The list elements can consits of a few elements. However the textarea makes the biggest difference.
+                </p>
 
                 <p>
-                    The list elements can consits of a few elements. However the text are makes the biggest difference.
+                    The list elements has three different styles that you can use. The styles that you can use are:
+                    <a href="#single-line">single line</a>, <a href="#two-line">two line</a> and <a href="#three-line">three line</a>.
                 </p>
             </section>
         </article>
@@ -25,7 +30,11 @@
             <h2 class="xs12" id="single-line">Single line</h2>
 
             <section class="xs12 sm6">
-                <ul class="list red">
+
+            </section>
+
+            <section class="xs12 sm6">
+                <ul class="list">
                     <li class="list--single-line">
                         <span class="list__content">Single line item</span>
                     </li>
@@ -67,7 +76,11 @@
             <h2 class="xs12" id="two-line">Two line</h2>
 
             <section class="xs12 sm6">
-                <ul class="list red">
+
+            </section>
+
+            <section class="xs12 sm6">
+                <ul class="list">
                     <li class="list--two-line">
                         <div class="list__content">
                             <span class="list__content__title">Two line item</span>
@@ -79,6 +92,108 @@
                             <span class="overline">Overline</span>
                             <span class="list__content__title">Secondary text</span>
                         </div>    
+                    </li>
+                    <li class="list--two-line list--support">
+                        <m-icon class="list__support__icon">person</m-icon>
+                        <div class="list__content">
+                            <span class="list__content__title">Two line item</span>
+                            <span class="list__content__text">Secondary text</span>
+                        </div>    
+                        <m-icon class="list__control">more_vert</m-icon>
+                    </li>
+                    <li class="list--two-line list--support">
+                        <div class="list__support__image">
+                            <img src="http://via.placeholder.com/40x40">
+                        </div>
+                        <div class="list__content">
+                            <span class="list__content__title">Two line item</span>
+                            <span class="list__content__text">Secondary text</span>
+                        </div>    
+                        <m-icon class="list__control">more_vert</m-icon>
+                    </li>
+                    <li class="list--two-line list--support">
+                        <div class="list__support__image">
+                            <img src="http://via.placeholder.com/40x40" class="shape--circle">
+                        </div>
+                        <div class="list__content">
+                            <span class="list__content__title">Two line item</span>
+                            <span class="list__content__text">Secondary text</span>
+                        </div>    
+                        <m-icon class="list__control">more_vert</m-icon>
+                    </li>
+                     <li class="list--two-line list--support">
+                        <div class="list__support__image--large">
+                            <img src="http://via.placeholder.com/100x56">
+                        </div>
+                        <div class="list__content">
+                            <span class="list__content__title">Two line item</span>
+                            <span class="list__content__text">Secondary text</span>
+                        </div>    
+                        <m-icon class="list__control">more_vert</m-icon>
+                    </li>
+                </ul>
+            </section>
+        </article>
+
+        <article class="row">
+            <h2 class="xs12" id="three-line">Three line</h2>
+
+            <section class="xs12 sm6">
+
+            </section>
+
+            <section class="xs12 sm6">
+                <ul class="list">
+                    <li class="list--three-line">
+                        <div class="list__content">
+                            <span class="list__content__title">Three line item</span>
+                            <span class="list__content__text">Secondary text Lorem ipsum dolor sit<br>amet, consectetu adipiscing elit.</span>
+                        </div>    
+                    </li>
+                    <li class="list--three-line">
+                        <div class="list__content">
+                            <span class="overline">Overline</span>
+                            <span class="list__content__title">Three line item</span>
+                            <span class="list__content__text">Secondary text Lorem ipsum dolor sit amet.</span>
+                        </div>    
+                    </li>
+                    <li class="list--three-line list--support">
+                        <m-icon class="list__support__icon">person</m-icon>
+                        <div class="list__content">
+                            <span class="list__content__title">Three line item</span>
+                            <span class="list__content__text">Secondary text Lorem ipsum dolor sit<br>amet, consectetu adipiscing elit.</span>
+                        </div>    
+                        <m-icon class="list__control">more_vert</m-icon>
+                    </li>
+                    <li class="list--three-line list--support">
+                        <div class="list__support__image">
+                            <img src="http://via.placeholder.com/40x40">
+                        </div>
+                        <div class="list__content">
+                            <span class="list__content__title">Three line item</span>
+                            <span class="list__content__text">Secondary text Lorem ipsum dolor sit<br>amet, consectetu.</span>
+                        </div>    
+                        <m-icon class="list__control">more_vert</m-icon>
+                    </li>
+                    <li class="list--three-line list--support">
+                        <div class="list__support__image">
+                            <img src="http://via.placeholder.com/40x40" class="shape--circle">
+                        </div>
+                        <div class="list__content">
+                            <span class="list__content__title">Three line item</span>
+                            <span class="list__content__text">Secondary text Lorem ipsum dolor sit<br>amet, consectetu.</span>
+                        </div>    
+                        <m-icon class="list__control">more_vert</m-icon>
+                    </li>
+                     <li class="list--three-line list--support">
+                        <div class="list__support__image--large">
+                            <img src="http://via.placeholder.com/100x56">
+                        </div>
+                        <div class="list__content">
+                            <span class="list__content__title">Two line item</span>
+                            <span class="list__content__text">Secondary text Lorem ipsum dolor sit<br>amet, consectetu.</span>
+                        </div>    
+                        <m-icon class="list__control">more_vert</m-icon>
                     </li>
                 </ul>
             </section>
